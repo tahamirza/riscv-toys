4a8eacde34b1f4bff97aaf07f94e02267c61d5e9
Taha Mirza <mail@tahamirza.com>
2018-11-18 16:35:01 -0500

    Hello, world!

--- test.s
+++ test.s
@@ -2,7 +2,25 @@
 	.globl _start
 _start:
 	nop
-	li x28, 0x10000000
-	li x29, 0x68
-	sd x29, (x28)
+	la a0, message
+	jal puts
+	nop
+	nop
 	ebreak
+
+	# writes the null terminated string pointed to by a0 to serial
+	# no return value
+puts:
+	li t0, 0x10000000 # address of serial
+puts_loop:
+	lb t1, (a0)
+	beqz t1, puts_done
+	sb t1, (t0)
+	addi a0, a0, 0x1
+	j puts_loop
+
+puts_done:
+	ret
+
+message:
+	.asciz "Hello, world!\n"
